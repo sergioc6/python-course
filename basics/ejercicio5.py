75b9dca32f546ac47c7b56e4f6a3328e70fc21cc
Sergio Cabral <sergiocabral.1990@gmail.com>
2020-12-07 18:20:30 -0300

    ejercicio 5

---
+++ basics/ejercicio5.py
@@ -0,0 +1,10 @@
+# Ejercicio 5
+
+total = float(input("Ingrese el Total de la compra: "))
+
+descuento = total * 0.15
+totalConDescuento = total - descuento
+
+print(f"El total de la compra es: ${total}")
+print(f"El descuento de la compra es: ${descuento}")
+print(f"El total con descuento de la compra es: ${totalConDescuento}")
